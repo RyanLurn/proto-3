023efaaeec2500b380fd9cca834a014be451fd40
Ryan Luong <lumintriet@gmail.com>
2025-09-10 19:58:47 +0700

    Set up Convex.

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,17 +1,20 @@
+import { ConvexClientProvider } from "@/components/providers/convex";
 import { ThemeProvider } from "@/components/providers/theme";
 import { Toaster } from "@/components/ui/sonner";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-      <Toaster closeButton richColors position="top-center" />
-    </ThemeProvider>
+    <ConvexClientProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster closeButton richColors position="top-center" />
+      </ThemeProvider>
+    </ConvexClientProvider>
   );
 }
 
