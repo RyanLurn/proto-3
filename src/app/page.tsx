fc76f285fcd04414e2a96fb142407550077a7ef0
Ryan Luong <lumintriet@gmail.com>
2025-09-10 19:24:37 +0700

    Add dark mode and test toast.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button onClick={() => toast.info("Hello")}>Click me</Button>
     </div>
   );
 }
