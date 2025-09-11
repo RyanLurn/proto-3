19b4caf69237208400b7b1ad181bfcdebf33c17c
Ryan Luong <lumintriet@gmail.com>
2025-09-11 17:49:17 +0700

    Link to tasks page in sidebar.

--- src/features/sidebar/components/main.tsx
+++ src/features/sidebar/components/main.tsx
@@ -1,13 +1,40 @@
 "use client";
 
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import Link from "next/link";
+import { ListTodo } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar";
 import { AppSidebarHeader } from "@/features/sidebar/components/header";
 
 function AppSidebar() {
   return (
     <Sidebar>
       <AppSidebarHeader />
-      <SidebarContent>Empty</SidebarContent>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Tools</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/tasks">
+                    <ListTodo />
+                    <span>Tasks Manager</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
   );
 }
